db79ba7481d219f2b631ebeb72059fcdf2b291c4
tangah2018 <605692458@qq.com>
2018-07-27 00:17:17 +0800

    将跨域访问屏蔽

--- src/main/java/com/shengruitn/config/CorsConfig.java
+++ src/main/java/com/shengruitn/config/CorsConfig.java
@@ -17,11 +17,11 @@ public class CorsConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowCredentials(true)
-                .allowedMethods("GET", "POST", "DELETE", "PUT")
-                .maxAge(3600);
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowCredentials(true)
+//                .allowedMethods("GET", "POST", "DELETE", "PUT")
+//                .maxAge(3600);
     }
 
 }
